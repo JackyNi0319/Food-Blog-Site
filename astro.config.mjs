c9655159a6f8ce407b1e1a747646e8f393d47873
Jacky <jackyni@yahoo.com>
2023-04-13 19:49:58 -0500

    Updated config file

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://jackyni0319.github.io',
-    base: '/Astro-LLP/second-star'
+    base: '/Food-Blog-Site'
 });
